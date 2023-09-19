47f0b8f1a223c84aeb345e41effe01153419d32c
wangyuan <wangyuan@qianlima.com>
2023-09-19 10:00:22 +0800

    feat: init

---
+++ rollup.config.js
@@ -0,0 +1,1 @@
+console.log('rollup', process.env.TARGET)
---
+++ scripts/build.js
@@ -0,0 +1,29 @@
+// 打包packages目录下的所有包
+
+const fs = require("fs");
+const execa = require("execa"); // 开启子进程打包，最终还是使用rollup打包
+
+const targets = fs.readdirSync("packages").filter((f) => {
+  if (fs.statSync(`packages/${f}`).isDirectory()) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+// 对我们的目标依次打包，并行打包
+
+async function build(target) {
+  await execa("rollup", ["-c", "--environment", `TARGET:${target}`], { stdio: "inherit" });
+}
+
+function runParallel(targets, iteratorFn) {
+  const res = [];
+  for (const item of targets) {
+    const p = iteratorFn(item);
+    res.push(p);
+  }
+  return Promise.all(res);
+}
+
+runParallel(targets, build);
