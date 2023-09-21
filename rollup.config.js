430a1b90ef5712dd01d95e8881bd1d613b17df1b
wangyuan <wangyuan@qianlima.com>
2023-09-21 11:51:26 +0800

    feat: 添加rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,13 @@
-console.log('rollup', process.env.TARGET)
+// rollup配置
+
+const path = require("path");
+
+// 根据环境变量中的target属性 获取对应模块中的 package.json
+const packagesDir = path.resolve(__dirname, "packages");
+const packageDir = path.resolve(packagesDir, process.env.TARGET);
+
+// 永远针对的是某个模块
+const resolve = (p) => path.resolve(packageDir, p);
+
+const pkg = require(resolve('package.json'))
+
